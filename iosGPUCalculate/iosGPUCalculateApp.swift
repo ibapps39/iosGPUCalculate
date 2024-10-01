5ceda75037ee724f7e0abac56e35ef70ad6d2020
Ian Brown <ibapps39@gmail.com>
2024-10-01 16:38:49 -0600

    Initial Commit

---
+++ iosGPUCalculate/iosGPUCalculateApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosGPUCalculateApp.swift
+//  iosGPUCalculate
+//
+//  Created by Ian Brown on 10/1/24.
+//
+
+import SwiftUI
+
+@main
+struct iosGPUCalculateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
